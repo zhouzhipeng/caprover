d97cd1f0fa8eceb62103271808ee674508256783
zhouzhipeng <ronnie@binance.com>
2020-09-14 16:22:50 +0800

    add notifications after build

--- src/user/ServiceManager.ts
+++ src/user/ServiceManager.ts
@@ -11,6 +11,8 @@ import ImageMaker, { BuildLogsManager } from './ImageMaker'
 import DomainResolveChecker from './system/DomainResolveChecker'
 import LoadBalancerManager from './system/LoadBalancerManager'
 import requireFromString = require('require-from-string')
+import request = require('request')
+
 
 const serviceMangerCache = {} as IHashMapGeneric<ServiceManager>
 
@@ -182,10 +184,12 @@ class ServiceManager {
                     )
             })
             .then(function () {
+                request("https://push.zhouzhipeng.com/uQNoctNcamuWivbLeWhkmT/发布通知/服务["+appName+"]发布成功✅")
                 self.onBuildFinished(appName)
                 return self.ensureServiceInitedAndUpdated(appName)
             })
             .catch(function (error) {
+                request("https://push.zhouzhipeng.com/uQNoctNcamuWivbLeWhkmT/发布通知/服务["+appName+"]发布失败❌")
                 self.onBuildFinished(appName)
                 return new Promise<void>(function (resolve, reject) {
                     self.logBuildFailed(appName, error)
